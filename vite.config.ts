4acd1ac35f1c032c2d9935d05a8e64d564b4ec36
Heaneycute <Heaneypower@gmail.com>
2025-02-23 17:47:42 +0300

    ii

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dismantling/', // Указываем путь к репозиторию на GitHub Pages
 })
