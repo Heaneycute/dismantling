37758684188431b4c06934de21db06180bf65502
Heaneycute <Heaneypower@gmail.com>
2025-02-26 16:10:02 +0300

    preparation for adaptation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,6 @@
 import React, { memo } from "react";
+import TG from "../assets/TG.png";
+import WA from "../assets/WA.png";
 import "../styles/HeroSection.css";
 
 const HeroSection: React.FC = memo(() => {
@@ -20,9 +22,40 @@ const HeroSection: React.FC = memo(() => {
           </h3>
         </div>
         <div className="hero-text">
-          <p>Ваш надёжный партнёр</p>
-          <p>в сфере демонтажа зданий</p>
-          <p>и сооружений любой сложности</p>
+          <p>
+            Ваш надёжный партнёр
+            <br />
+            в сфере демонтажа зданий
+            <br />и сооружений любой сложности
+          </p>
+          <div className="footer-bottom">
+            <div className="social-icons">
+              <a
+                href="https://t.me/Snos_SPB"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Telegram компании"
+              >
+                <div className="social-circle telegram">
+                  <img src={TG} alt="Иконка Telegram" loading="lazy" />
+                </div>
+              </a>
+              <a
+                href="https://wa.me/qr/TUAHMMTAHWAQG1"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="WhatsApp компании"
+              >
+                <div className="social-circle whatsapp">
+                  <img src={WA} alt="Иконка WhatsApp" loading="lazy" />
+                </div>
+              </a>
+            </div>
+            <div className="contact-info">
+              <p>+79119271855</p>
+              <p>9271855@mail.ru</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
